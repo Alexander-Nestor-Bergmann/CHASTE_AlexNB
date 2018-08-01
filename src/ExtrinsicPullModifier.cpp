9cc7418d3fe38cd0a25ac9e5a59fe895e93228f7
Alexander Nestor-Bergmann <alexnb@dhcp-10-249-9-29.eduroam.wireless.private.cam.ac.uk>
2018-08-01 16:30:17 +0100

    Additional edit to RHS boundary code

--- src/ExtrinsicPullModifier.cpp
+++ src/ExtrinsicPullModifier.cpp
@@ -45,166 +45,166 @@ void ExtrinsicPullModifier<DIM>::UpdateAtEndOfTimeStep(AbstractCellPopulation<DI
     }
     else
     {
-        // // Get the centroid of the boundary nodes
-        // // Define variables for centroid position
-        // double centroidX = 0;
-        // double centroidY = 0;
-        // double counter = 0;
-        // // Sum the X,Y coords for all points
-        // for (unsigned node_index=0; node_index<num_nodes; node_index++)
-        // {
-        //     Node<DIM>* tempNode = rCellPopulation.GetNode(node_index);
-        //
-        //     if (tempNode->IsBoundaryNode())
-        //     {
-        //         centroidX += tempNode->rGetLocation()[0];
-        //         centroidY += tempNode->rGetLocation()[1];
-        //         counter += 1;
-        //     }
-        // }
-        // // Divide by total to get centroid.
-        // centroidX /= counter;
-        // centroidY /= counter;
-        //
-        // // Make a vector of node indices:
-        // std::vector< int > boundaryNodes;
-        // for (unsigned node_index=0; node_index<num_nodes; node_index++)
-        // {
-        //     // Get the node at this index
-        //     Node<DIM>* p_node = rCellPopulation.GetNode(node_index);
-        //
-        //     // If the node is a boundary node, order and store it.
-        //     if (p_node->IsBoundaryNode())
-        //     {
-        //         // Get the X and Y coords
-        //         double currentX = p_node->rGetLocation()[0];
-        //         double currentY = p_node->rGetLocation()[1];
-        //
-        //         // If this is the first boundary node, just add it to the list.
-        //         if (boundaryNodes.size() == 0)
-        //         {
-        //             boundaryNodes.push_back(node_index);
-        //         }
-        //         // else, compare it to the current nodes; put it infront of
-        //         // the first node that makes a larger angle, relative to
-        //         // centroid.
-        //         else
-        //         {
-        //             int i = 0; // counter
-        //             bool done = false; // Breaks when we have placed the vertex
-        //             while( not done )
-        //             {
-        //                 // If we have reached the end of the list, break
-        //                 if (i == boundaryNodes.size())
-        //                 {
-        //                     boundaryNodes.push_back(node_index);
-        //                     done = true;
-        //                 }
-        //
-        //                 // Get the x and y coords from the next node in
-        //                 // boundaryNodes to compare against.
-        //                 Node<DIM>* tempNode = rCellPopulation.GetNode(boundaryNodes[i]);
-        //                 // Get the coords of the node at the current index in
-        //                 // the ordered list of boundary nodes.
-        //                 double previousX = tempNode->rGetLocation()[0];
-        //                 double previousY = tempNode->rGetLocation()[1];
-        //
-        //                 // Calculate the angle that the two nodes make relative
-        //                 // to the centroid
-        //                 double prevAngle;
-        //                 double dY = previousY - centroidY;
-        //                 double dX = previousX - centroidX;
-        //                 prevAngle = std::atan2( dY, dX );
-        //                 double currentAngle;
-        //                 dX = currentX - centroidX;
-        //                 dY = currentY - centroidY;
-        //                 currentAngle = std::atan2(dY, dX);
-        //                 // If the angle of new node is smaller, it comes first
-        //                 // so append it at this location.
-        //                 if ( currentAngle < prevAngle && not done )
-        //                 {
-        //                     boundaryNodes.insert(boundaryNodes.begin() + i, node_index);
-        //                     done = true;
-        //                 }
-        //
-        //                 // update count
-        //                 i += 1;
-        //             }
-        //         }
-        //     }
-        // }
-        //
-        // // Get the top and bottom rightmost corners of bounding box.
-        // double lowerX = bounds.rGetUpperCorner()[0];
-        // double lowerY = bounds.rGetLowerCorner()[1];
-        // double upperX = bounds.rGetUpperCorner()[0];
-        // double upperY = bounds.rGetUpperCorner()[1];
-        //
-        // // Make variables to hold indices of nodes closest to bounding box.
-        // int upperNode = 0;
-        // int lowerNode = 0;
-        // // Euclid distane variables
-        // double dist2;
-        // double upperDist = 10000000.; // Initialise as huge
-        // double lowerDist = 10000000.; // Initialise as huge
-        // double tempDist;
-        // double xDiff;
-        // double yDiff;
-        // for (unsigned i=0; i < boundaryNodes.size(); i++)
-        // {
-        //     // Get the node pointer
-        //     Node<DIM>* tempNode = rCellPopulation.GetNode(boundaryNodes[i]);
-        //
-        //     // calc dist to upper corner
-        //     xDiff = upperX - tempNode->rGetLocation()[0];
-        //     yDiff = upperY - tempNode->rGetLocation()[1];
-        //     dist2 = xDiff*xDiff + yDiff*yDiff;
-        //     tempDist = std::sqrt(dist2);
-        //     // If smaller, mark this as the closest node.
-        //     if( tempDist < upperDist )
-        //     {
-        //         upperDist = tempDist;
-        //         upperNode = i;
-        //     }
-        //
-        //     // calc dist to lower corner
-        //     xDiff = lowerX - tempNode->rGetLocation()[0];
-        //     yDiff = lowerY - tempNode->rGetLocation()[1];
-        //     dist2 = xDiff*xDiff + yDiff*yDiff;
-        //     tempDist = std::sqrt(dist2);
-        //     // If smaller, mark this as the closest node.
-        //     if( tempDist < lowerDist )
-        //     {
-        //         lowerDist = tempDist;
-        //         lowerNode = i;
-        //     }
-        // }
-        //
-        // // Iterate over the rightmost nodes and pull them.
-        // for (int i=0; i < boundaryNodes.size(); i++)
-        // {
-        //     // If it lies between the top and bottom:
-        //     if( i >= lowerNode && i <= upperNode)
-        //     {
-        //         Node<DIM>* p_node = rCellPopulation.GetNode(boundaryNodes[i]);
-        //         p_node->rGetModifiableLocation()[0] += mSpeed*dt;
-        //     }
-        //     else if( i > upperNode)
-        //     {
-        //         break;
-        //     }
-        // }
-
-
-        // Pull on the right-most nodes only, with a constant speed
+        // Get the centroid of the boundary nodes
+        // Define variables for centroid position
+        double centroidX = 0;
+        double centroidY = 0;
+        double counter = 0;
+        // Sum the X,Y coords for all points
         for (unsigned node_index=0; node_index<num_nodes; node_index++)
         {
-            Node<DIM>* p_node = rCellPopulation.GetNode(node_index);
-            if (fabs(p_node->rGetLocation()[0] - x_max) < 0.1)
+            Node<DIM>* tempNode = rCellPopulation.GetNode(node_index);
+
+            if (tempNode->IsBoundaryNode())
             {
-                p_node->rGetModifiableLocation()[0] += mSpeed*dt;
+                centroidX += tempNode->rGetLocation()[0];
+                centroidY += tempNode->rGetLocation()[1];
+                counter += 1;
             }
         }
+        // Divide by total to get centroid.
+        centroidX /= counter;
+        centroidY /= counter;
+
+        // Make a vector of node indices:
+        std::vector< int > boundaryNodes;
+        for (unsigned node_index=0; node_index<num_nodes; node_index++)
+        {
+            // Get the node at this index
+            Node<DIM>* p_node = rCellPopulation.GetNode(node_index);
+
+            // If the node is a boundary node, order and store it.
+            if (p_node->IsBoundaryNode())
+            {
+                // Get the X and Y coords
+                double currentX = p_node->rGetLocation()[0];
+                double currentY = p_node->rGetLocation()[1];
+
+                // If this is the first boundary node, just add it to the list.
+                if (boundaryNodes.size() == 0)
+                {
+                    boundaryNodes.push_back(node_index);
+                }
+                // else, compare it to the current nodes; put it infront of
+                // the first node that makes a larger angle, relative to
+                // centroid.
+                else
+                {
+                    int i = 0; // counter
+                    bool done = false; // Breaks when we have placed the vertex
+                    while( not done )
+                    {
+                        // If we have reached the end of the list, break
+                        if (i == boundaryNodes.size())
+                        {
+                            boundaryNodes.push_back(node_index);
+                            done = true;
+                        }
+
+                        // Get the x and y coords from the next node in
+                        // boundaryNodes to compare against.
+                        Node<DIM>* tempNode = rCellPopulation.GetNode(boundaryNodes[i]);
+                        // Get the coords of the node at the current index in
+                        // the ordered list of boundary nodes.
+                        double previousX = tempNode->rGetLocation()[0];
+                        double previousY = tempNode->rGetLocation()[1];
+
+                        // Calculate the angle that the two nodes make relative
+                        // to the centroid
+                        double prevAngle;
+                        double dY = previousY - centroidY;
+                        double dX = previousX - centroidX;
+                        prevAngle = std::atan2( dY, dX );
+                        double currentAngle;
+                        dX = currentX - centroidX;
+                        dY = currentY - centroidY;
+                        currentAngle = std::atan2(dY, dX);
+                        // If the angle of new node is smaller, it comes first
+                        // so append it at this location.
+                        if ( currentAngle < prevAngle && not done )
+                        {
+                            boundaryNodes.insert(boundaryNodes.begin() + i, node_index);
+                            done = true;
+                        }
+
+                        // update count
+                        i += 1;
+                    }
+                }
+            }
+        }
+
+        // Get the top and bottom rightmost corners of bounding box.
+        double lowerX = bounds.rGetUpperCorner()[0];
+        double lowerY = bounds.rGetLowerCorner()[1];
+        double upperX = bounds.rGetUpperCorner()[0];
+        double upperY = bounds.rGetUpperCorner()[1];
+
+        // Make variables to hold indices of nodes closest to bounding box.
+        int upperNode = 0;
+        int lowerNode = 0;
+        // Euclid distane variables
+        double dist2;
+        double upperDist = 10000000.; // Initialise as huge
+        double lowerDist = 10000000.; // Initialise as huge
+        double tempDist;
+        double xDiff;
+        double yDiff;
+        for (unsigned i=0; i < boundaryNodes.size(); i++)
+        {
+            // Get the node pointer
+            Node<DIM>* tempNode = rCellPopulation.GetNode(boundaryNodes[i]);
+
+            // calc dist to upper corner
+            xDiff = upperX - tempNode->rGetLocation()[0];
+            yDiff = upperY - tempNode->rGetLocation()[1];
+            dist2 = xDiff*xDiff + yDiff*yDiff;
+            tempDist = std::sqrt(dist2);
+            // If smaller, mark this as the closest node.
+            if( tempDist < upperDist )
+            {
+                upperDist = tempDist;
+                upperNode = i;
+            }
+
+            // calc dist to lower corner
+            xDiff = lowerX - tempNode->rGetLocation()[0];
+            yDiff = lowerY - tempNode->rGetLocation()[1];
+            dist2 = xDiff*xDiff + yDiff*yDiff;
+            tempDist = std::sqrt(dist2);
+            // If smaller, mark this as the closest node.
+            if( tempDist < lowerDist )
+            {
+                lowerDist = tempDist;
+                lowerNode = i;
+            }
+        }
+
+        // Iterate over the rightmost nodes and pull them.
+        for (int i=0; i < boundaryNodes.size(); i++)
+        {
+            // If it lies between the top and bottom:
+            if( i >= lowerNode && i <= upperNode)
+            {
+                Node<DIM>* p_node = rCellPopulation.GetNode(boundaryNodes[i]);
+                p_node->rGetModifiableLocation()[0] += mSpeed*dt;
+            }
+            else if( i > upperNode)
+            {
+                break;
+            }
+        }
+
+
+        // // Pull on the right-most nodes only, with a constant speed
+        // for (unsigned node_index=0; node_index<num_nodes; node_index++)
+        // {
+        //     Node<DIM>* p_node = rCellPopulation.GetNode(node_index);
+        //     if (fabs(p_node->rGetLocation()[0] - x_max) < 0.1)
+        //     {
+        //         p_node->rGetModifiableLocation()[0] += mSpeed*dt;
+        //     }
+        // }
 
 
 
